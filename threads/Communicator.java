9512c3a64a6c5b2a6edb3d01e455fb13b846b2f2
RonaldPS1993 <72328178+RonaldPS1993@users.noreply.github.com>
2020-10-25 12:26:49 -0400

    Add files via upload

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -10,32 +10,40 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
-    /**
-     * Allocate a new communicator.
-     */
+    private Condition speakReady;
+    private Condition listenReady;
+    private int speaking = 0, listening = 0;
+    private int messenger;
+    private Lock lock;
+	
     public Communicator() {
+    	lock = new Lock();
+    	speakReady = new Condition(lock);
+    	listenReady = new Condition(lock);
+    	
     }
-
-    /**
-     * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
-     *
-     * <p>
-     * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
-     *
-     * @param	word	the integer to transfer.
-     */
+    
     public void speak(int word) {
-    }
-
-    /**
-     * Wait for a thread to speak through this communicator, and then return
-     * the <i>word</i> that thread passed to <tt>speak()</tt>.
-     *
-     * @return	the integer transferred.
-     */    
+    	lock.acquire();
+    	speaking++;
+    	while(listening == 0){
+    		listenReady.sleep();
+    	}
+    	listening--;
+    	messenger = word;
+    	speakReady.wake();
+    	lock.release();
+    } 
     public int listen() {
-	return 0;
+    lock.acquire();
+    listening++;
+    listenReady.wake();
+    while(speaking == 0){
+    	speakReady.sleep();
+    }
+    speaking--;
+    int result = messenger;
+    lock.release();
+	return result;
     }
 }
